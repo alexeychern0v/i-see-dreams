b7b543f9d20c70018b5a0eec7fe4c9c222771d5b
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-10 15:03:49 +0200

    Started main page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>I See Dreams</h1>
+      <p>Record your dreams, uncover their meaning, and receive a daily phrase to guide your day. Dive deep into your inner world.</p>
+      <button onClick={() => alert("Analyze will start soon 🙂")}>Interpret my dream</button>
+    </div>
+  );
+}
+
+export default App;
