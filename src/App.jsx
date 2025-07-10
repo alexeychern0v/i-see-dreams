4ad27d8d6190aa01a2f6618e06cbadb402825479
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-10 15:05:33 +0200

    Added app style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="container">
       <h1>I See Dreams</h1>
-      <p>Record your dreams, uncover their meaning, and receive a daily phrase to guide your day. Dive deep into your inner world.</p>
+      <p>Record your dreams, uncover their meaning, and receive a daily phrase to guide your day. </p>
+      <p>Dive deep into your inner world.</p>
       <button onClick={() => alert("Analyze will start soon 🙂")}>Interpret my dream</button>
     </div>
   );
