f44f6b277cadd331400962f4ca1790c30e80b7ce
alexeychern0v <alexchernov063@gmail.com.com>
2025-07-10 16:44:37 +0200

    Added const dreams to futher save all user's dreams and DreamForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import { useState } from 'react';
+import DreamForm from './components/DreamForm';
 
 function App() {
+  const [dreams, setDreams] = useState([]);
+
+  const handleSave = (entry) => {
+    setDreams([entry, ...dreams]);
+    // DB later
+  };
+  
   return (
     <div className="container">
       <h1>I See Dreams</h1>
       <p>Record your dreams, uncover their meaning, and receive a daily phrase to guide your day. </p>
       <p>Dive deep into your inner world.</p>
-      <button onClick={() => alert("Analyze will start soon 🙂")}>Interpret my dream</button>
+      <DreamForm onSave={handleSave} />
     </div>
   );
 }
